f6e96df5d402b73de2dbb95ba63aaea400afad5d
Konstantin Omelnytskyi <omelnit@ukr.net>
2020-04-20 18:27:24 +0300

    the begin

---
+++ components/Autoload.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ * Функция __autoload для автоматического подключения классов
+ */
+function my_autoloader($class_name)
+// function __autoload($class_name)
+{
+    // Массив папок, в которых могут находиться необходимые классы
+    $array_paths = array(
+        '/models/',
+        '/components/',
+        '/controllers/',
+    );
+
+    // Проходим по массиву папок
+    foreach ($array_paths as $path) {
+
+        // Формируем имя и путь к файлу с классом
+        $path = ROOT . $path . $class_name . '.php';
+
+        // Если такой файл существует, подключаем его
+        if (is_file($path)) {
+            include_once $path;
+        }
+    }
+}
+spl_autoload_register('my_autoloader');
+// {
+//     # List all the class directories in the array.
+//     $array_paths = array(
+//         '/models/',
+//         '/components/',
+//         '/controllers/',
+//     );
+
+//     foreach ($array_paths as $path) {
+//         $path = ROOT . $path . $class_name . '.php';
+//         if (is_file($path)) {
+//             include_once $path;
+//         }
+//     }
+// }
